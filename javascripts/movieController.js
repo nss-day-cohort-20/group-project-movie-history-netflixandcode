1d004dd2f613c9af5c7cc239518517df37dd4c52
Sam Staff <samstaff@Sams-MacBook-Pro.local>
2017-07-20 15:12:22 -0500

    Filter search functionality

--- javascripts/movieController.js
+++ javascripts/movieController.js
@@ -3,11 +3,12 @@
 
 let $ = require('jquery');
 let api = require('./api-getter.js');
-let showcaseMovies = require('./showcaseMovies.js'); 
+let showcaseMovies = require('./showcaseMovies.js');
 let movieFactory = require('./movieFactory.js');
-
+let searchBtn = $('#searchButton');
 let $container = $('.container');
 let templates = require('./template-builder');
+let movieSearch = $('#movie');
 
 // function buildMovieObj() {
 //     let movieObj = {
@@ -19,9 +20,59 @@ let templates = require('./template-builder');
 // let movieArray = [];
 
 module.exports.activateEL = () => {
-    $('#searchButton').click(movieFactory.getMovies);
+    searchBtn.click(movieFactory.getMovies);
+    $('#untracked').click(() =>{
+	    clearFilter();
+    	movieSearch.addClass('utr');
+		});//toggle untracked
+    $('#unwatched').click(() =>{
+	    clearFilter();
+	    movieSearch.addClass('uwt');
+	    });//toggle unwatched
+    $('#watched').click(() =>{
+	    clearFilter();
+	    movieSearch.addClass('wtc');
+	    });//toggle watched
+    $('#favorites').click(() =>{
+	    clearFilter();
+	    movieSearch.addClass('fav');
+	    });//toggle favorites
+    $('.addTracked').click(() =>{
+//run addtracked TODO: addtracked functionality
+    });
 };
 
+movieSearch.keypress((event) =>{
+	if (event.keyCode === 13){
+			clearDOM();
+			movieFactory.getMovies();//this shouldn't be in the final version, just here for testing.
+		if (movieSearch.hasClass('utr') === true){
+
+	        // TODO: run specific filtered searches here.
+		} else if (movieSearch.hasClass('uwt') === true){
+
+
+		} else if (movieSearch.hasClass('wtc') === true){
+
+
+		} else if (movieSearch.hasClass('fav') === true){
+
+
+		} else {
+			alert('Error: Filter not toggled on');
+		}
+	}
+});
+
+
+
+function clearDOM(){
+    $('#container').empty();
+}
+
+function clearFilter(){
+    movieSearch.removeClass('utr uwt wtc fav');
+}
 
 // module.exports.getMovies = () => {
 //     return new Promise( (resolve, reject) => {
@@ -33,7 +84,7 @@ module.exports.activateEL = () => {
 //             type: 'GET',
 //             url: searchURL,
 //             contentType: 'application/json'
-        
+
 //     }).done( (data) => {
 //         console.log("data", data);
 //         let temp = data.results;
@@ -74,7 +125,9 @@ module.exports.activateEL = () => {
 
 
 
-
+        // if (searchBtn.hasClass('utr') === false) {// use if clear filter isn't enough
+        //     searchBtn.addClass('utr');
+        // } //toggle untracked//just run clear filter
 
 
 
@@ -91,8 +144,3 @@ module.exports.activateEL = () => {
 //         });
 //     };
 // };
-
-
-
-
-      
