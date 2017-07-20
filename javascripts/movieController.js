42ec7e024213dbcdfea10da412894703ecbb3a2a
Joshua Meyers <joshua.r.meyers@gmail.com>
2017-07-20 15:27:27 -0500

    combined all other functionality with materialize items. templates and materialize made.

--- javascripts/movieController.js
+++ javascripts/movieController.js
@@ -9,6 +9,8 @@ let movieFactory = require('./movieFactory.js');
 let $container = $('.container');
 let templates = require('./template-builder');
 
+let searchBtn = $('#searchButton');
+let movieSearch = $('#movie');
 // function buildMovieObj() {
 //     let movieObj = {
 //         title: $("#form--title").val()
@@ -18,9 +20,61 @@ let templates = require('./template-builder');
 
 // let movieArray = [];
 
-module.exports.activateEL = () => {
-    $('#searchButton').click(movieFactory.getMovies);
+module.exports.activateEL = () => {    
+    // $('#searchButton').click(movieFactory.getMovies);
+        searchBtn.click(movieFactory.getMovies);
+    $('.untracked').click(() =>{
+       clearFilter();
+       movieSearch.addClass('utr');
+       });//toggle untracked
+    $('.unwatched').click(() =>{
+       clearFilter();
+       movieSearch.addClass('uwt');
+       });//toggle unwatched
+    $('.watched').click(() =>{
+       clearFilter();
+       movieSearch.addClass('wtc');
+       });//toggle watched
+    $('.favorites').click(() =>{
+       clearFilter();
+       movieSearch.addClass('fav');
+       });//toggle favorites
+    $('.addTracked').click(() =>{
+//run addtracked TODO: addtracked functionality
+    });
 };
+
+movieSearch.keypress((event) =>{
+   if (event.keyCode === 13){
+           clearDOM();
+           movieFactory.getMovies();//this shouldn't be in the final version, just here for testing.
+       if (movieSearch.hasClass('utr') === true){
+
+           // TODO: run specific filtered searches here.
+       } else if (movieSearch.hasClass('uwt') === true){
+
+
+       } else if (movieSearch.hasClass('wtc') === true){
+
+
+       } else if (movieSearch.hasClass('fav') === true){
+
+
+       } else {
+           alert('Error: Filter not toggled on');
+       }
+   }
+});
+
+
+
+function clearDOM(){
+    $('#container').empty();
+}
+
+function clearFilter(){
+    movieSearch.removeClass('utr uwt wtc fav');
+}
 
 
 // module.exports.getMovies = () => {
--- javascripts/showcaseMovies.js
+++ javascripts/showcaseMovies.js
@@ -4,6 +4,7 @@
 let Handlebars = require('hbsfy/runtime');
 let movieTemp = require('./template-builder'); 
 let $ = require('jquery');
+let altPoster = '../imgs/noposter.jpg';
 // let $container = $('#container');
 // let container = document.getElementById("container");
 
@@ -12,15 +13,21 @@ let $ = require('jquery');
 
 module.exports.movieObjBuilder = (movieData, actors) => {
 	// finalMovieData = [];
-	console.log("movie obj actors", actors);
+	// console.log("movie obj actors", actors);
 	// console.log("moviedata", movieData);
-	for (let i = 0; i < movieData.length; i++) {
+	for (let i = 0; i < movieData.length; i++) {		
 		movieData[i].rating = 0;
 		movieData[i].watched = false;
-		movieData[i].cast = actors.name;
+		movieData[i].cast = actors[i];
+		if (movieData[i].poster_path === null) {
+			movieData[i].poster_path = altPoster;
+		} else {
+			movieData[i].poster_path = (`https://image.tmdb.org/t/p/w500` + movieData[i].poster_path);
+		}
 	}
-	console.log("did it work?", movieData);
-	$("#container").html(movieTemp.makeMovieList(movieData));
+
+	// console.log("did it work?", movieData);
+	$("#container").append(movieTemp.makeMovieList(movieData));
 }; 
 
 
