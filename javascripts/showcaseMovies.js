9ea4e593a8a0afbe099644d8de057136d0a0ac17
Joshua Meyers <joshua.r.meyers@gmail.com>
2017-07-21 09:09:02 -0500

    added some comments, a bit to styling

--- javascripts/showcaseMovies.js
+++ javascripts/showcaseMovies.js
@@ -1,28 +1,25 @@
 'use strict';
 
-// let templates = require('./template-builder'); 
+ 
 let Handlebars = require('hbsfy/runtime');
 let movieTemp = require('./template-builder'); 
 let $ = require('jquery');
 let altPoster = '../imgs/noposter.jpg';
-// let $container = $('#container');
-// let container = document.getElementById("container");
 
-// let movie = [];
-// moviedata[i].cast = actordata[i].cast
 
 module.exports.movieObjBuilder = (movieData, actors) => {
-	// finalMovieData = [];
 	// console.log("movie obj actors", actors);
 	// console.log("moviedata", movieData);
+	// append this data to movieData
 	for (let i = 0; i < movieData.length; i++) {		
 		movieData[i].rating = 0;
 		movieData[i].watched = false;
 		movieData[i].cast = actors[i];
+		//if there is no poster, a specefic image will display instead
 		if (movieData[i].poster_path === null) {
 			movieData[i].poster_path = altPoster;
 		} else {
-			movieData[i].poster_path = (`https://image.tmdb.org/t/p/w500` + movieData[i].poster_path);
+			movieData[i].poster_path = (`https://image.tmdb.org/t/p/w342` + movieData[i].poster_path);
 		}
 	}
 
@@ -31,23 +28,3 @@ module.exports.movieObjBuilder = (movieData, actors) => {
 }; 
 
 
-
-
-
-		// let movieList = templates.makeMovieList(movie[i]);
-		// $container.html(movieList);
-
-		// $("#container").append(movieTemp(movie));
-
-
-
-
-		// movie.id = movieData[i].id;
-		// movie.title = movieData[i].title;
-		// movie.year = movieData[i].release_date;
-		// movie.image = movieData[i].poster_path;
-		// movie.rating = 0;
-		// movie.watched = false;
-		// movie.actors = null;
-		// console.log("its working", movie);
-		// $container.innerHtml += `<div>Title: ${movieData[i].title}</div>`;
