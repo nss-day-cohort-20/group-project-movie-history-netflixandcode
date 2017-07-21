8f7486652605bc6db70e5bda9ba443c7f51a9955
Emily Lemmon <eclaire.lemmon@gmail.com>
2017-07-21 15:58:04 -0500

    start on FB factory

--- javascripts/showcaseMovies.js
+++ javascripts/showcaseMovies.js
@@ -13,17 +13,10 @@ let firebase = require('./firebaseConfig.js');
 
 
 module.exports.movieObjBuilder = (movieData, actors) => {
-
-	let currentUser = firebase.auth().currentUser.uid;
-	console.log("current User", currentUser);
-	// finalMovieData = [];
-	// console.log("movie obj actors", actors);
-	// console.log("moviedata", movieData);
 	for (let i = 0; i < movieData.length; i++) {
 		movieData[i].rating = 0;
 		movieData[i].watched = false;
 		movieData[i].cast = actors[i];
-		movieData[i].user = currentUser;		
 
 		if (movieData[i].poster_path === null) {
 			movieData[i].poster_path = altPoster;
